c4be1f5e119909d19e7b18c3c063f6da5618c773
John Karahalis <john.karahalis@gmail.com>
2015-05-19 14:16:36 -0500

    Add Gulp task to beautify JavaScript

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var beautify = require('gulp-jsbeautifier');
+
+gulp.task('beautify', ['beautify:javascript']);
+
+gulp.task('beautify:javascript', function() {
+    gulp.src(['*.js', '*.json']).pipe(beautify({
+        indentSize: 4,
+        keepFunctionIndentation: true
+    })).pipe(gulp.dest('./'));
+});
 
 gulp.task('test', ['test:jshint']);
 
