62f0c059efb8df7c440bf1cb4d369ded781db72a
John Karahalis <john.karahalis@gmail.com>
2015-06-25 14:40:07 -0400

    Refactor homepage
    These changes give us a static homepage. Some benefits over the original
    implementation include validation, a working title, and keeping
    substantial HTML out of JavaScript.

--- gulpfiles/dev.js
+++ gulpfiles/dev.js
@@ -1,22 +1,22 @@
 'use strict';
 
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
 var beautify = require('gulp-jsbeautifier');
+var jshint = require('gulp-jshint');
 
 gulp.task('beautify', ['beautify:javascript']);
 
 gulp.task('beautify:javascript', function() {
-    gulp.src(['*.js', '*.json']).pipe(beautify({
+    gulp.src(['../*.js', '../*.json']).pipe(beautify({
         indentSize: 4,
         keepFunctionIndentation: true
-    })).pipe(gulp.dest('./'));
+    })).pipe(gulp.dest('../'));
 });
 
 gulp.task('test', ['test:jshint']);
 
 gulp.task('test:jshint', function() {
-    gulp.src('*.js')
+    gulp.src('../*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
