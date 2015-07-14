40ce89eaf7900c74fa1e4d8451ae863228236132
John Karahalis <john.karahalis@gmail.com>
2015-07-14 17:47:30 -0400

    Fix #104: Rate-limit comment submissions

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 var config = {
     userAgent: 'Discord',
     token: process.env.OAUTH_TOKEN,
+    commentWait: process.env.COMMENT_WAIT || 50,
 
     // Used when no environment variable (process.env.PORT) is set
     port: 3000,
