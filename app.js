b01dbb7e0b21fc1a92ec4b4fe42c1c42eeb7235e
John Karahalis <john.karahalis@gmail.com>
2015-07-15 14:45:30 -0400

    Stop using deprecated bodyParser constructor
    This resolves the following warnings we were seeing in test output:
    
        body-parser deprecated bodyParser: use individual json/urlencoded middlewares app.js:15:9
        body-parser deprecated undefined extended: provide extended option node_modules/body-parser/index.js:75:29

--- app.js
+++ app.js
@@ -2,20 +2,27 @@
 
 var path = require('path');
 
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var hook = require('./hook');
 var config = require('./config');
 var logger = require('./logger');
 
-// Set up Express
-var express = require('express');
-var bodyParser = require('body-parser');
 var port = process.env.PORT || config.port;
 
+// Set up Express
 var app = express();
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// Serve the homepage
 app.use(express.static(path.join(__dirname, 'build/www')));
+
+// Handle webhook POSTs
 app.post('/hook', hook.handle);
 
 exports.listener = app.listen(port);
-
 logger.info('Listening at port:', port);
