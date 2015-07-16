30695378c9522ce6ab1093a89eae675a8f948d55
David Walsh <davidwalsh83@gmail.com>
2015-07-16 09:12:16 -0500

    Restructure processing logic

--- processor.js
+++ processor.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var path = require('path');
+
 var doiuse = require('doiuse');
 var postcss = require('postcss');
 var sourceMap = require('source-map');
@@ -7,6 +9,29 @@ var stylus = require('stylus');
 
 var logger = require('./logger');
 var utils = require('./utils');
+
+
+
+/**
+ * Router for processing files CSS and Stylus
+ */
+function process(githubClient, repo, branch, file, config, handleIncompatibility) {
+    var processor;
+
+    switch (path.extname(file.filename).toLowerCase()) {
+        case '.css':
+            processor = processCSS;
+            break;
+        case '.styl':
+            processor = processStylus;
+            break;
+        default:
+            return;
+    }
+
+    return processor.apply(null, arguments);
+}
+
 
 /**
  * Test and report on changes to the given CSS stylesheet.
@@ -22,16 +47,10 @@ function processCSS(githubClient, repo, branch, file, config, handleIncompatibil
 
         fileContents = utils.prepareContent(fileContentsMetadata.content);
 
-        // Test the stylesheet with doiuse and call handleIncompatibility for
-        // any incompatibilities that are found
-        postcss(doiuse({
-            browsers: config,
-            onFeatureUsage: handleIncompatibility
-        })).process(fileContents, {
-            from: '/' + filename
-        }).then(); // onFeatureUsage won't be called unless then() is called.
+        return _postcss(config, fileContents, handleIncompatibility, filename);
     });
 }
+
 
 /**
  * Test and report on changes to the given Stylus stylesheet.
@@ -67,18 +86,25 @@ function processStylus(githubClient, repo, branch, file, config, handleIncompati
                 handleIncompatibility(incompatibility);
             }
 
-            // Test the stylesheet with doiuse and call
-            // handleStylusIncompatibility for any incompatibilities that are
-            // found
-            postcss(doiuse({
-                browsers: config,
-                onFeatureUsage: handleStylusIncompatibility
-            })).process(compiledCSS, {
-                from: '/' + filename
-            }).then(); // onFeatureUsage won't be called unless then() is called.
+            return _postcss(config, fileContents, handleStylusIncompatibility, filename);
         });
     });
 }
 
-exports.processCSS = processCSS;
-exports.processStylus = processStylus;
+
+/**
+ * Runs postcss for each of the processors
+ */
+function _postcss(config, contents, handle, filename) {
+    // Test the stylesheet with doiuse and call handleIncompatibility for
+    // any incompatibilities that are found
+    return postcss(doiuse({
+        browsers: config,
+        onFeatureUsage: handle
+    })).process(contents, {
+        from: '/' + filename
+    }).then(); // onFeatureUsage won't be called unless then() is called.
+}
+
+
+exports.process = process;
